e226bad302d2550a0625e28ac9aa4b21caf6eeb3
diadomraz <diadomraz@users.noreply.github.com>
2023-06-24 09:51:57 +0300

    Update IITC-Ingress-potential-L8.js
    Add namespace

--- IITC-Ingress-potential-L8.js
+++ IITC-Ingress-potential-L8.js
@@ -5,6 +5,7 @@
 // @version        0.3
 // @description    Hides any portal lower than level 7 and allows to check/record resonator status.
 // @id             IITC-Ingress-potential-L8@diadomraz
+// @namespace      https://github.com/diadomraz/IITC-Ingress-potential-L8/
 // @updateURL      https://raw.githubusercontent.com/diadomraz/IITC-Ingress-potential-L8/main/IITC-Ingress-potential-L8.js
 // @downloadURL    https://raw.githubusercontent.com/diadomraz/IITC-Ingress-potential-L8/main/IITC-Ingress-potential-L8.js
 // @match          https://intel.ingress.com/*
